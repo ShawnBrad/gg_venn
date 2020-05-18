376f14a3c307e2a71311bba93b930cb35f2756a1
Shawn Williams <shawnwilliams@shawns-mbp.lan>
2020-05-18 13:13:42 -0400

    add step to check that input is a list and ensure list names are present

--- R/gg_venn.R
+++ R/gg_venn.R
@@ -1,5 +1,8 @@
 #' @export
 gg_venn<- function(overlap, ...){
+  
+  if (class(overlap) != 'list') stop('please enter a list')
+  if (is.na(names(overlap))) names(overlap)= c('A','B','C','D')[1:length(overlap)]
 
   if (length(overlap) == 2){
     setup.list(overlap = overlap) %>%
