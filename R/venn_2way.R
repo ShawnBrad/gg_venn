8f3551b161535838982bc02a5a0ac1c0f2af38e8
Shawn Williams <shawnwilliams@shawns-mbp.lan>
2020-07-31 15:23:38 -0400

    reworked method to determine label cordinates to one based on the circle intercept and radi

--- R/venn_2way.R
+++ R/venn_2way.R
@@ -21,25 +21,46 @@ venn_2way <- function(overlap.df, overlap, label.col,label.size ){
    if(min(len.list) < .1 ){
      len.list[which(len.list == min(len.list))] = .1
    }
+  
+  # determine x coordinates 
+  
+  x.cords = c(0, 0 + len.list[1])
+  y.cords = c(0, 0)
 
   # create df to draw circles
-  df.venn <- data.frame(x = c(0, 1)- min(len.list)/4,
-                        y = c(0, 0),
+  df.venn <- data.frame(x =  x.cords,
+                        y = y.cords,
                         venn.names = names(overlap))
+  
+  
+  intercept <- circles.intersect(r = len.list, x = x.cords, y = y.cords)
+  
+  #circles.intersect(r = c(1,1), x = c(0, (0 + 1)), y = c(0,0))
+  
+  # get coords for labels 
+  x.labs.cord = c((x.cords[1] - len.list[1]) + len.list[1]/2,  intercept[1],
+                  (x.cords[2] + len.list[2]) - len.list[2]/2)
+                  
+  
+    
+    
+  #circles.intersect(r = c(1,.5),x = c(0,.5), y = c(0,0))
 
 
   overlap.df <- as.data.frame(overlap.df[-1,]) %>%
-    mutate(x = c(1.2+min(len.list)/4, .95 - min(len.list)/2, 0 - min(len.list)/2), y = c(0, 0, 0))
+    mutate(x =  x.labs.cord, y = c(0, 0, 0))
 
   # reverse x positions depending on which list is larger
-  if(len.list[1] < len.list[2]){
-    overlap.df$x = .95+ (overlap.df$x * -1)
-  }
+  # if(len.list[1] < len.list[2]){
+  #   overlap.df$x = .95+ (overlap.df$x * -1)
+  # }
 
 
 # plot venn
   base_venn <- ggplot(df.venn, aes(x0 = x, y0 = y, r = len.list, fill = venn.names)) +
     geom_circle(alpha = .5, size = 1, colour = 'grey50') +
+    
+    
     annotate("text", x = overlap.df$x, y = overlap.df$y, 
              label = overlap.df$Counts[c(1,3,2)], 
              size = 6, color = label.col[c(1,2,3)])
